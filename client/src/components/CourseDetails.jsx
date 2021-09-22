7d6fdf8d00cef94ce2d404fe2b6bbf35270f8a5c
utsavd03 <85575604+utsavd03@users.noreply.github.com>
2021-09-22 22:31:16 +0530

    added course page

--- client/src/components/CourseDetails.jsx
+++ client/src/components/CourseDetails.jsx
@@ -1,56 +1,212 @@
-import React from 'react';
+import React from "react";
 
 function CourseDetails() {
-   return (
-      <div class='courseMainDiv'>
-         <div class='courseDiv1'>
-            <h2>Courses Offered</h2>
-            <h6>
-               Some of the courses offered for full stack web devlopment...
-            </h6>
+  return (
+    <div class="courseMainDiv">
+      <div class="courseDiv1">
+        <h2>Courses Offered</h2>
+        <h6>Some of the courses offered for full stack web devlopment...</h6>
 
-            <div class='indiCourse'>
-               <div class='row' style={{ margin: '0' }}>
-                  <div
-                     class='col-lg-4 photoSection'
-                     style={{ backgroundColor: '#ffa089' }}
-                  >
-                     <img src='./images/html (1).png' />
-                  </div>
-                  <div class='col-lg-8 descriptionSection'>
-                     <h4>Html 5</h4>
-                     <p>Course Duration: 10+ hours</p>
-                     <p>
-                        This course is for both beginners and seasoned
-                        developers that want to learn how to build responsive
-                        websites and user interfaces with modern HTML5{' '}
-                     </p>
-                  </div>
-               </div>
+        <div class="indiCourse">
+          <div class="row" style={{ margin: "0" }}>
+            <div
+              class="col-lg-4 photoSection"
+              style={{ backgroundColor: "#ffa089" }}
+            >
+              <img src="./images/html5.png" />
             </div>
+            <div class="col-lg-8 descriptionSection">
+              <h4>Html 5</h4>
+              <p class="duration">
+                Course Duration: <span>10+ hours</span>
+              </p>
+              <p>
+                This course is for both beginners and seasoned developers that
+                want to learn how to build responsive websites and user
+                interfaces with modern HTML5{" "}
+              </p>
+            </div>
+          </div>
+        </div>
 
-            <div class='indiCourse'>
-               <div class='row' style={{ margin: '0' }}>
-                  <div
-                     class='col-lg-4 photoSection'
-                     style={{ backgroundColor: '#87cefa' }}
-                  >
-                     <img src='./images/css-3.png' />
-                  </div>
-                  <div class='col-lg-8 descriptionSection'>
-                     <h4>Css 3</h4>
-                     <p>Course Duration: 10+ hours</p>
-                     <p>
-                        This course is for both beginners and seasoned
-                        developers that want to learn how to build responsive
-                        websites and user interfaces with modern HTML5{' '}
-                     </p>
-                  </div>
-               </div>
+        <div class="indiCourse">
+          <div class="row" style={{ margin: "0" }}>
+            <div
+              class="col-lg-4 photoSection"
+              style={{ backgroundColor: "#87cefa" }}
+            >
+              <img src="./images/css3.png" />
             </div>
-         </div>
+            <div class="col-lg-8 descriptionSection">
+              <h4>CSS 3</h4>
+              <p class="duration">
+                Course Duration: <span>10+ hours</span>
+              </p>
+              <p>
+                This course is for both beginners and seasoned developers that
+                want to learn how to build responsive websites and user
+                interfaces with modern HTML5{" "}
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div class="indiCourse">
+          <div class="row" style={{ margin: "0" }}>
+            <div
+              class="col-lg-4 photoSection"
+              style={{ backgroundColor: "#87cefa" }}
+            >
+              <img src="./images/bootstrap.png" />
+            </div>
+            <div class="col-lg-8 descriptionSection">
+              <h4>Bootstrap</h4>
+              <p class="duration">
+                Course Duration: <span>10+ hours</span>
+              </p>
+              <p>
+                This course is for both beginners and seasoned developers that
+                want to learn how to build responsive websites and user
+                interfaces with modern HTML5{" "}
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div class="indiCourse">
+          <div class="row" style={{ margin: "0" }}>
+            <div
+              class="col-lg-4 photoSection"
+              style={{ backgroundColor: "#fbec5d" }}
+            >
+              <img src="./images/js.png" />
+            </div>
+            <div class="col-lg-8 descriptionSection">
+              <h4>Javascript</h4>
+              <p class="duration">
+                Course Duration: <span>10+ hours</span>
+              </p>
+              <p>
+                This course is for both beginners and seasoned developers that
+                want to learn how to build responsive websites and user
+                interfaces with modern HTML5{" "}
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div class="indiCourse">
+          <div class="row" style={{ margin: "0" }}>
+            <div
+              class="col-lg-4 photoSection"
+              style={{ backgroundColor: "#74c365" }}
+            >
+              <img src="./images/nodejs.png" />
+            </div>
+            <div class="col-lg-8 descriptionSection">
+              <h4>Node JS</h4>
+              <p class="duration">
+                Course Duration: <span>10+ hours</span>
+              </p>
+              <p>
+                This course is for both beginners and seasoned developers that
+                want to learn how to build responsive websites and user
+                interfaces with modern HTML5{" "}
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div class="indiCourse">
+          <div class="row" style={{ margin: "0" }}>
+            <div
+              class="col-lg-4 photoSection"
+              style={{ backgroundColor: "#d3d3d3" }}
+            >
+              <img src="./images/express.png" />
+            </div>
+            <div class="col-lg-8 descriptionSection">
+              <h4>Express JS</h4>
+              <p class="duration">
+                Course Duration: <span>10+ hours</span>
+              </p>
+              <p>
+                This course is for both beginners and seasoned developers that
+                want to learn how to build responsive websites and user
+                interfaces with modern HTML5{" "}
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div class="indiCourse">
+          <div class="row" style={{ margin: "0" }}>
+            <div
+              class="col-lg-4 photoSection"
+              style={{ backgroundColor: "#696969" }}
+            >
+              <img src="./images/react.png" />
+            </div>
+            <div class="col-lg-8 descriptionSection">
+              <h4>React JS</h4>
+              <p class="duration">
+                Course Duration: <span>10+ hours</span>
+              </p>
+              <p>
+                This course is for both beginners and seasoned developers that
+                want to learn how to build responsive websites and user
+                interfaces with modern HTML5{" "}
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div class="indiCourse">
+          <div class="row" style={{ margin: "0" }}>
+            <div
+              class="col-lg-4 photoSection"
+              style={{ backgroundColor: "#353839" }}
+            >
+              <img src="./images/angular.png" />
+            </div>
+            <div class="col-lg-8 descriptionSection">
+              <h4>Angular JS</h4>
+              <p class="duration">
+                Course Duration: <span>10+ hours</span>
+              </p>
+              <p>
+                This course is for both beginners and seasoned developers that
+                want to learn how to build responsive websites and user
+                interfaces with modern HTML5{" "}
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div class="indiCourse">
+          <div class="row" style={{ margin: "0" }}>
+            <div
+              class="col-lg-4 photoSection"
+              style={{ backgroundColor: "#d0f0c0" }}
+            >
+              <img src="./images/mongodb.png" />
+            </div>
+            <div class="col-lg-8 descriptionSection">
+              <h4>MongoDB</h4>
+              <p class="duration">
+                Course Duration: <span>10+ hours</span>
+              </p>
+              <p>
+                This course is for both beginners and seasoned developers that
+                want to learn how to build responsive websites and user
+                interfaces with modern HTML5{" "}
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
-   );
+    </div>
+  );
 }
 
 export default CourseDetails;
